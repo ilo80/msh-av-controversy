5d0ddd357834f609da2e67a8d0bd17125720b200
ilo80 <ilo80.pro@gmail.com>
2025-06-09 18:57:33 +0200

    chlore(introduction): replace placeholder description

--- src/components/Introduction.tsx
+++ src/components/Introduction.tsx
@@ -5,7 +5,7 @@ function Introduction() {
         VOITURES AUTONOMES :<br />LA FIN DE LA ROUTE POUR LES HUMAINS ?
       </h1>
       <p className="introduction-description">
-        Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.
+       À l’heure où l’intelligence artificielle révolutionne nos modes de vie, les voitures deviennent elles aussi intelligentes, capables de se déplacer sans intervention humaine. Ces technologies promettent un meilleur confort et une réduction des accidents. Mais derrière ces avancées se cachent de nombreux enjeux : Qui devient responsable en cas d’accident ? Ces voitures sont-elles vraiment sûres face aux menaces informatiques extérieures ? Leur système de conduite est-il vraiment fiable ?
       </p>
     </section>
   )
