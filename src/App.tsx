5154bb0d83ec3fb87096a5f7116c97a588bd2f5e
Ilann Mimet <ilann.mimet@gmail.com>
2025-06-09 18:16:16 +0200

    feat(app): add full page introduction

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,12 @@
 
 import ActorsGraph from './components/ActorsGraph'
+import Introduction from './components/Introduction'
 
 function App() {
   
   return (
     <div className="container">
-      <div className="content-container">
-        <h1>Bienvenue</h1>
-        <p>Ceci est un site sur la controverse des voitures autonomes</p>
-      </div>
+      <Introduction />
       <ActorsGraph />
     </div>
   )
