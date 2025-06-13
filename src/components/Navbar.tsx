dc720c845ce51bf63c7a530c429385923ddb6541
ilo80 <ilo80.pro@gmail.com>
2025-06-13 07:08:54 +0200

    chlore(navbar): replace navbar title by logo

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,14 +1,16 @@
+import logo from "../assets/logo/logo.svg"
 
 function Navbar() {
   return (
     <nav id="navbar" className="navbar">
-      <h1 className="navbar-title">La revue du savoir</h1>
-        <ul className="navbar-links">
-            <li><a href="#introduction">Introduction</a></li>
-            <li><a href="#actors">Acteurs</a></li>
-            <li><a href="#timeline">Chronologie</a></li>
-            <li><a href="#graph">Cartographie</a></li>
-        </ul>
+      {/* <h1 className="navbar-title">La revue du savoir</h1> */}
+      <img src={logo} alt="Logo" style={{ width: '100px' }} />
+      <ul className="navbar-links">
+          <li><a href="#introduction">Introduction</a></li>
+          <li><a href="#actors">Acteurs</a></li>
+          <li><a href="#timeline">Chronologie</a></li>
+          <li><a href="#graph">Cartographie</a></li>
+      </ul>
     </nav>
   )
 }
