554a9fd5b85c02597862e050d1a44abcec2e032f
ilo80 <ilo80.pro@gmail.com>
2025-06-16 23:28:47 +0200

    feat(navbar): add scroll animation and reset scroll on item click

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,15 +1,28 @@
 import logo from "../assets/logo/logo.svg"
 
 function Navbar() {
+  const handleClick = (e: React.MouseEvent<HTMLAnchorElement>) => {
+    e.preventDefault();
+
+    const targetId = e.currentTarget.getAttribute("href");
+    if (!targetId) return;
+
+    const targetElement = document.querySelector(targetId);
+    if (!targetElement) return;
+
+    targetElement.scrollIntoView({ behavior: "smooth" });
+    document.body.style.overflow = ""; // Reset overflow to default
+  }
+
   return (
     <nav id="navbar" className="navbar">
       {/* <h1 className="navbar-title">La revue du savoir</h1> */}
       <img src={logo} alt="Logo" style={{ width: '100px' }} />
       <ul className="navbar-links">
-          <li><a href="#introduction">Introduction</a></li>
-          <li><a href="#actors">Acteurs</a></li>
-          <li><a href="#timeline">Chronologie</a></li>
-          <li><a href="#graph">Cartographie</a></li>
+          <li><a href="#introduction" onClick={handleClick}>Introduction</a></li>
+          <li><a href="#actors" onClick={handleClick}>Acteurs</a></li>
+          <li><a href="#timeline" onClick={handleClick}>Chronologie</a></li>
+          <li><a href="#graph" onClick={handleClick}>Cartographie</a></li>
       </ul>
     </nav>
   )
