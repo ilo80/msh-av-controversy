60c2688994f112f1de858d14a3eb48a2272bc26c
ilo80 <ilo80.pro@gmail.com>
2025-06-17 18:54:21 +0200

    chlore(introduction): justify introduction

--- src/components/Introduction.tsx
+++ src/components/Introduction.tsx
@@ -1,12 +1,14 @@
 function Introduction() {
   return (
     <section id="introduction" className="introduction-container">
-      <h1 className="introduction-title">
-        VOITURES AUTONOMES :<br />LA FIN DE LA ROUTE POUR LES HUMAINS ?
-      </h1>
-      <p className="introduction-description">
-       À l’heure où l’intelligence artificielle révolutionne nos modes de vie, les voitures deviennent elles aussi intelligentes, capables de se déplacer sans intervention humaine. Ces technologies promettent un meilleur confort et une réduction des accidents. Mais derrière ces avancées se cachent de nombreux enjeux : Qui devient responsable en cas d’accident ? Ces voitures sont-elles vraiment sûres face aux menaces informatiques extérieures ? Leur système de conduite est-il vraiment fiable ?
-      </p>
+      <div className="introduction-content">
+        <h1 className="introduction-title">
+          VOITURES AUTONOMES :<br />LA FIN DE LA ROUTE POUR LES HUMAINS ?
+        </h1>
+        <p className="introduction-description">
+        À l’heure où l’intelligence artificielle révolutionne nos modes de vie, les voitures deviennent elles aussi intelligentes, capables de se déplacer sans intervention humaine. Ces technologies promettent un meilleur confort et une réduction des accidents. Mais derrière ces avancées se cachent de nombreux enjeux : Qui devient responsable en cas d’accident ? Ces voitures sont-elles vraiment sûres face aux menaces informatiques extérieures ? Leur système de conduite est-il vraiment fiable ?
+        </p>
+      </div>
     </section>
   )
 }
