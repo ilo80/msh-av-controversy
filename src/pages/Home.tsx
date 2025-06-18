a76735250294266f3dc23fce38f5616dc0a6e427
ilo80 <ilo80.pro@gmail.com>
2025-06-18 21:46:05 +0200

    chlore: change components order

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -15,9 +15,9 @@ function Home() {
       <IntroductionSection />
       <Terminology />
       <ActorSection />
+      <ActorsGraph />
       <TimelineSection />
       <StatisticsSection />
-      <ActorsGraph />
       <Footer />
     </div>
   )
