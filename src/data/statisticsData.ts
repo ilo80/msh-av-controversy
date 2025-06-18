f2686bd34082ec0b607ad8b6c1e61debdebe7085
lordrandy <139745395+lordrandy@users.noreply.github.com>
2025-06-18 22:38:32 +0200

    [UPDATE] statisticsData.ts pour PatateDouce

--- src/data/statisticsData.ts
+++ src/data/statisticsData.ts
@@ -4,25 +4,25 @@ const statisticsData: Statistic[] = [
     {
         id: 'us-human-error',
         value: '94',
-        description: 'des accidents de la route sont dus à une erreur humaine aux États-Unis',
+        description: 'Nombre d\'accidents de la route dus à une erreur humaine aux États-Unis',
         type: 'percentage'
     },
     {
         id: 'tesla-deaths',
-        value: '50 décès',
-        description: 'déjà enregistrés impliquant le système Autopilot de Tesla (niveaux 2) depuis 2016',
+        value: '50',
+        description: 'Nombre de décès déjà enregistrés impliquant le système Autopilot de Tesla (niveaux 2) depuis 2016',
         type: 'default'
     },
     {
         id: 'tesla-distance-between-crashes',
-        value: '~12,3 millions de km',
-        description: 'entre deux accidents lors de l\'usage du système Autopilot de Tesla en 2024',
+        value: '12,3 millions de km',
+        description: 'Distance moyenne entre deux accidents lors de l\'usage du système Autopilot de Tesla en 2024',
         type: 'default'
     },
     {
         id: 'waymo-rides-weekly',
         value: '~200 000',
-        description: 'courses en robot-taxi sont effectuées chaque semaine par Waymo en 2025',
+        description: 'Nombre de courses en robot-taxi effectuées chaque semaine par Waymo en 2025',
         type: 'default'
     },
     {
@@ -34,13 +34,13 @@ const statisticsData: Statistic[] = [
     {
         id: 'fr-confidence',
         value: '67',
-        description: 'des Français n’ont pas confiance dans les voitures autonomes en 2023',
+        description: 'Part des Français qui n’ont pas confiance dans les voitures autonomes en 2023',
         type: 'percentage'
     },
     {
         id: 'us-opposition',
         value: '68',
-        description: 'part des Américains qui se disent opposés aux voitures autonomes en 2023',
+        description: 'Part des Américains qui se disent opposés aux voitures autonomes en 2023',
         type: 'percentage'
     }
 ];
