e2c8de59d5035ba4cb2775dd46c0945eecdb01cd
lordrandy <139745395+lordrandy@users.noreply.github.com>
2025-06-18 20:31:30 +0200

    [UPDATE] Add moral machine link

--- src/components/Introduction.tsx
+++ src/components/Introduction.tsx
@@ -9,6 +9,10 @@ function Introduction() {
         À l’heure où l’intelligence artificielle révolutionne nos modes de vie, les voitures deviennent elles aussi intelligentes, capables de se déplacer sans intervention humaine. Ces technologies promettent un meilleur confort et une réduction des accidents. Mais derrière ces avancées se cachent de nombreux enjeux : Qui devient responsable en cas d’accident ? Ces voitures sont-elles vraiment sûres face aux menaces informatiques extérieures ? Leur système de conduite est-il vraiment fiable ?
         </p>
       </div>
+      
+      <div className="moral-div">
+        <a href="https://moralmachine.net">Et vous êtes-vous moral ?</a>
+      </div>
     </section>
   )
 }
