6c2c1c0cad21a90ed1f9cb04a1ed14dbdc8aa943
ilo80 <ilo80.pro@gmail.com>
2025-06-18 09:13:41 +0200

    refactor(router): move home page to pages/ directory

--- src/main.tsx
+++ src/main.tsx
@@ -2,11 +2,12 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import { createBrowserRouter, RouterProvider } from 'react-router-dom'
 
+import Home from './pages/Home.tsx'
+
 import './index.css'
-import App from './App.tsx'
 
 const router = createBrowserRouter([
-  {path: '/', element: <App />},
+  {path: '/', element: <Home />},
 ])
 
 createRoot(document.getElementById('root')!).render(
